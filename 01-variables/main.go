8dd9cc29800043e8044002659573d684375f20aa
Joan Rojas <sebas.rojas.mejia@gmail.com>
2025-08-24 20:35:15 -0500

    bytes lesson

--- 01-variables/main.go
+++ 01-variables/main.go
@@ -1,49 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"unsafe"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// declaramos una variable con var
+	var myIntVar int
+	myIntVar = -12
+	fmt.Println("My variable is: ", myIntVar)
 
-	var myIntVar int;
-	myIntVar = -42;
-	fmt.Println("My variable is set to", myIntVar);
-	fmt.Println("myIntVar's address is", &myIntVar);
+	// variable uint, solo numeros positivos
+	var myUintVar uint
+	myUintVar = 12
+	fmt.Println("My variable is: ", myUintVar)
 
-	var myBoolVar bool;
-	myBoolVar = true;
-	fmt.Println("My variable is set to", myBoolVar);
-	fmt.Println("myBoolVar's address is", &myBoolVar);
+	// variable string
+	var myStringVar string
+	myStringVar = "My string variable"
+	fmt.Println("My variable is: ", myStringVar)
 
-	var myStringVar string;
-	myStringVar = "Hello, World!";
-	fmt.Println("My variable is set to", myStringVar);
-	fmt.Println("myStringVar's address is", &myStringVar);
+	// variable booleana
+	var myBoolVar bool
+	myBoolVar = true
+	fmt.Println("My variable is: ", myBoolVar)
 
-	var myFloatVar float64;
-	myFloatVar = 22.2;
-	fmt.Println("My variable is set to", myFloatVar);
-	fmt.Println("myFloatVar's address is", &myFloatVar);
+	// vemos la direccion de memoria de nuestra variable
+	fmt.Println("My variable address is: ", &myStringVar)
 
-	var myComplexVar complex128;
-	myComplexVar = 1 + 2i;
-	fmt.Println("My variable is set to", myComplexVar);
-	fmt.Println("myComplexVar's address is", &myComplexVar);
+	// go tomara el valor que asignamos como tipo de dato (al usar :=)
+	myIntVar2 := 12
+	fmt.Println("My variable is: ", myIntVar2)
 
-	myIntVar2 := 43;
-	fmt.Println("My variable is set to", myIntVar2);
-	fmt.Println("myIntVar2's address is", &myIntVar2);
+	myStringVar2 := "My string variable with :="
+	fmt.Println(myStringVar2)
 
-	myBoolVar2 := false;
-	fmt.Println("My variable is set to", myBoolVar2);
-	fmt.Println("myBoolVar2's address is", &myBoolVar2);
+	myBoolVar2 := true
+	fmt.Println("My variable is: ", myBoolVar2)
 
-	myStringVar2 := "Hello, World!";
-	fmt.Println("My variable is set to", myStringVar2);
-	fmt.Println("myStringVar2's address is", &myStringVar2);
+	fmt.Println()
 
-	const myConstVar int = 44;
-	fmt.Println("My variable is set to", myConstVar);
+	// la constante tomara el valor asignado como tipo de dato
+	const myFirstConst = "a12"
+	fmt.Println("My const is: ", myFirstConst)
+
+	// especificamos el tipo de dato de la constante
+	const myIntConst int = 12
+	fmt.Println("My const is: ", myIntConst)
+
+	const myStringConst string = "aaa"
+	fmt.Println("My const is: ", myStringConst)
 
 	/*
 		int8	8 bits	-128 to 127
@@ -53,6 +61,102 @@ func main() {
 		int	Platform dependent	Platform dependent
 	*/
 
-	var myInt8Var int8 = 127;
-	fmt.Printf("My variable is set to %d and its address is %p\n", myInt8Var, &myInt8Var);
+	fmt.Println()
+
+	var myVar int
+	myVar = 23
+	fmt.Printf("%T", myVar) // imprime tipo de dato (int)
+	fmt.Printf("%d", myVar) // imprime valor (23)
+	// %d int, %f float, %s string
+
+	var my8BitsIntVar int8
+	// valor por defecto 0
+	fmt.Printf("Int default value: %d\n", my8BitsIntVar)
+
+	// unsafe.Sizeof nos devuelve el tamaño en bytes
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my8BitsIntVar, unsafe.Sizeof(my8BitsIntVar), unsafe.Sizeof(my8BitsIntVar)*8)
+
+	var my16BitsIntVar int16
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my16BitsIntVar, unsafe.Sizeof(my16BitsIntVar), unsafe.Sizeof(my16BitsIntVar)*8)
+
+	var my32BitsIntVar int32
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my32BitsIntVar, unsafe.Sizeof(my32BitsIntVar), unsafe.Sizeof(my32BitsIntVar)*8)
+
+	var my64BitsIntVar int64
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my64BitsIntVar, unsafe.Sizeof(my64BitsIntVar), unsafe.Sizeof(my64BitsIntVar)*8)
+
+	/*
+		Type	Size	Range
+		uint8	8 bits	0 to 255
+		uint16	16 bits	0 to 2^16 -1
+		uint32	32 bits	0 to 2^32 -1
+		uint64	64 bits	0 to 2^64 -1
+		uint	Platform dependent	Platform dependent
+	*/
+
+	fmt.Println()
+
+	var my8BitsUnitVar uint8
+	fmt.Printf("Uint default value: %d\n", my8BitsUnitVar)
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my8BitsUnitVar, unsafe.Sizeof(my8BitsUnitVar), unsafe.Sizeof(my8BitsUnitVar)*8)
+
+	var my16BitsUnitVar uint16
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my16BitsUnitVar, unsafe.Sizeof(my16BitsUnitVar), unsafe.Sizeof(my16BitsUnitVar)*8)
+
+	var my32BitsUnitVar uint32
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my32BitsUnitVar, unsafe.Sizeof(my32BitsUnitVar), unsafe.Sizeof(my32BitsUnitVar)*8)
+
+	var my64BitsUnitVar uint64
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", my64BitsUnitVar, unsafe.Sizeof(my64BitsUnitVar), unsafe.Sizeof(my64BitsUnitVar)*8)
+
+	fmt.Println()
+
+	var myFloast32Var float32
+	fmt.Printf("Float default value: %f\n", myFloast32Var)
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", myFloast32Var, unsafe.Sizeof(myFloast32Var), unsafe.Sizeof(myFloast32Var)*8)
+	fmt.Println()
+
+	var myFloat64Var float64
+	fmt.Printf("Float default value: %f\n", myFloat64Var)
+	fmt.Printf("type: %T, bytes: %d, bits: %d\n", myFloat64Var, unsafe.Sizeof(myFloat64Var), unsafe.Sizeof(myFloat64Var)*8)
+	fmt.Println()
+
+	// string es una secuencia de bytes
+	var myStringVar3 string
+	fmt.Printf("String default value: %s\n", myStringVar3)
+
+	myStringVar5 := `My string variable in golang
+	with multiple
+	line`
+
+	fmt.Printf("The variable value is: %s\n", myStringVar5)
+
+	// con las llaves definimos un scope
+	{
+		// conversiones
+		fmt.Println()
+		floatVar := 33.11
+		fmt.Printf("type: %T, value: %f\n", floatVar, floatVar)
+		floatStrVar := fmt.Sprintf("%.2f", floatVar)
+		fmt.Printf("type: %T, value: %s\n", floatStrVar, floatStrVar)
+
+		intVar := 22
+		fmt.Printf("type: %T, value: %d\n", intVar, intVar)
+		intStrVar := fmt.Sprintf("%d", intVar)
+		fmt.Printf("type: %T, value: %s\n", intStrVar, intStrVar)
+
+		intVal1, err := strconv.ParseInt("1333", 0, 64)
+		fmt.Println(err)
+		fmt.Printf("type: %T, value: %d\n", intVal1, intVal1)
+
+		intVal2, err := strconv.ParseInt("aa122", 0, 64)
+		fmt.Println(err)
+		fmt.Printf("type: %T, value: %d\n", intVal2, intVal2)
+
+		floatVar1, _ := strconv.ParseFloat("-11.2", 64)
+		fmt.Printf("type: %T, value: %f\n", floatVar1, floatVar1)
+
+	}
+
+	fmt.Println()
 }
